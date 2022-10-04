35b3a827d1d94a4ec726d87b7b7fb3ef286a2470
Jared McMahan <jaredm788@gmail.com>
2022-10-03 18:12:21 -0600

    added forEach and added likeBtn

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ This will be overview of project. detail what the project is and its purpose
 * Fork and clone
 * Run json server for db.json
 * Open index.html in the browser 
+
+
+references: https://stackoverflow.com/questions/71740975/how-can-i-retrieve-and-loop-through-data-retrieved-from-a-fetch-request
--- javascript/index.js
+++ javascript/index.js
@@ -1,107 +1,132 @@
-// function handleRequest() {
 
-//    fetch('https://api.openbrewerydb.org/breweries')
-//     .then(response => response.json())
-//     .then(response => console.log("response",response))
-//     .catch(error => console.log("error", error)) 
+//fetch data and and append results from search based on city or state
+document.getElementById('beer-search').addEventListener('click', function(e){
+  let citySearch = document.getElementById("city-input").value
+  const beerApi = `https://api.openbrewerydb.org/breweries?by_city=${encodeURIComponent(citySearch)}`;// make sure there's no spaces in city name
+  
+  console.log("search", citySearch)
+  fetch(beerApi)
+       .then(response => response.json())
+       .then(data => {
+            console.log("click", data) 
+            
+            document.getElementById('clearBrew').innerHTML = ""
+
+            data.forEach(renderBeerCard)
+
+            
+       });
+
+})
+
+function renderBeerCard (drink, index, allBrews) {
+  console.log(drink, index, allBrews)
+
+    let drinkCard = document.createElement('div')
+    let brewName = document.createElement('h3')
+    let brewType = document.createElement("p")
+    let likeBtn = document.createElement("button")
+    likeBtn.addEventListener("click", (e) => {
+      likeBrew(drink)
+    })
+
+    brewName.innerHTML = drink.name
+    brewType.innerHTML = drink.brewery_type
+
+      drinkCard.append(brewName,brewType, likeBtn)
+      document.getElementById('clearBrew').append(drinkCard)
+}
+
+function likeBrew(drink) {
+  console.log("-----",drink)
+}
+
     
-
 // }
+// request()
 
-// handleRequest() 
+// stack over flow reference
+// const request = async (cityName) => {
+//   const beerApi = `https://api.openbrewerydb.org/breweries?by_city=${encodeURIComponent(cityName)}`;
+//   const res = await fetch(beerApi);
+//   if (res.ok) {
+//     const breweries = await res.json();
+//     breweries.forEach(brewery => console.log(brewery));
+//   } else {
+//     console.log("Error!");
+//   }
+// };
+
+//   document.addEventListener("DOMContentLoaded", e => {
+//   const searchBtn = document.getElementById("bsearch");
+
+//   searchBtn.addEventListener("click", async function (e) {
+//     e.preventDefault;
+//     let searchQuery = document.getElementById("beer-search").value;
+  
+//     await request(searchQuery);
+//   });
+// })
 
 
-// create new element and append li to div container
-// function newElements(drink) {
-// const container = document.querySelector("#list")
-// const li = document.createElement("li")
-// container.append(li)
-// li.textContent = `${request} api`
-// }
+
+
+
+
+
+
+
+//everything below works together to render api data to page 
 
 //request api data and parse response into JSON
-function request(drink) {
-    fetch("https://api.openbrewerydb.org/breweries")
-    .then((response) => response.json())
-    .then((data) => renderBeer(data))
+// function request(drink) {
+//     fetch("https://api.openbrewerydb.org/breweries")
+//     .then((response) => response.json())
+//     .then((data) => renderBeer(data))
     
-}
-request()
-
-// function to grab all the values in each object.
-// function grabWholeObj() {
-  
+    
 // }
+// request()
 
+// use function below for single obj grab. 
 
-function renderBeer(drink){
-  console.log(drink) // this is v. useful to see what the data looks like!
-  // create DOM elements
-  let drinkCard = document.createElement('div')
-  let brewName = document.createElement('h3')
-  let brewType = document.createElement("p")
-  let brewStreet = document.createElement("p")
-  let brewCity = document.createElement("p")
-  let brewState = document.createElement("p")
-  let brewCountry = document.createElement("p")
-  let brewPostalCode = document.createElement("p")
-  let brewURL = document.createElement("p")
-  // //the data is an array of objects, this is picking the first item in the array
-  // //you can also use Math.random if you don't just want the first item
+// function renderBeer(drink){
+//   console.log(drink) // this is v. useful to see what the data looks like!
+//   // create DOM elements
+//   let drinkCard = document.createElement('div')
+//   let brewName = document.createElement('h3')
+//   let brewType = document.createElement("p")
+//   let brewStreet = document.createElement("p")
+//   let brewCity = document.createElement("p")
+//   let brewState = document.createElement("p")
+//   let brewCountry = document.createElement("p")
+//   let brewPostalCode = document.createElement("p")
+//   let brewURL = document.createElement("p")
+//   // //the data is an array of objects, this is picking the first item in the array
+//   // //you can also use Math.random if you don't just want the first item
   
-  // add DOM attributes
-  drinkCard.id = `drink - ${drink.id}`
-  drinkCard.className = "drink-card"
-  //Noam code help below
-  brewName.innerHTML = drink[0].name
-  brewType.innerHTML = drink[0].brewery_type
-  brewStreet.innerHTML = drink[0].street
-  brewCity.innerHTML = drink[0].city
-  brewState.innerHTML = drink[0].state
-  brewCountry.innerHTML = drink[0].country
-  brewPostalCode.innerHTML = drink[0].postal_code
-  brewURL.innerHTML = drink[0].website_url
+//   // add DOM attributes
+//   drinkCard.id = `drink - ${drink.id}`
+//   drinkCard.className = "drink-card"
+//   //Noam code help below
+//   brewName.innerHTML = drink[0].name
+//   brewType.innerHTML = drink[0].brewery_type
+//   brewStreet.innerHTML = drink[0].street
+//   brewCity.innerHTML = drink[0].city
+//   brewState.innerHTML = drink[0].state
+//   brewCountry.innerHTML = drink[0].country
+//   brewPostalCode.innerHTML = drink[0].postal_code
+//   brewURL.innerHTML = "url", drink[0].website_url
 
-
-  drinkCard.append(brewName,brewType, brewStreet, brewCity, brewState, brewCountry, brewPostalCode, brewURL)
-  document.getElementById('brewContainer').append(drinkCard)
-}
+//   // properly append DOM elements
+//   drinkCard.append(brewName,brewType, brewStreet, brewCity, brewState, brewCountry, brewPostalCode, brewURL)
+//   document.getElementById('brewContainer').append(drinkCard)
+// }
 
 //check answer:
 // function init() {
 // .forEach(beerObj => renderBeer(beerObj))
-
 // }
-
-
-//use this renderBeer function below if I get stuck on creating a new one
-// function renderBeer(drink){
-//   console.log(drink) // this is v. useful to see what the data looks like!
-//   let drinkCard = document.createElement('div')
-//   let name = document.createElement('h2')
-//   let type = document.createElement("li")
-
-//   drinkCard.id = `drink - ${drink.id}`
-//   name.textContent = drink.name
-
-
-//   //the data is an array of objects, this is picking the first item in the array
-//   //you can also use Math.random if you don't just want the first item
-//   name.innerHTML = drink[10].name
-//   drinkCard.append(name,type)
-//   document.getElementById('list').append(drinkCard)
-// }
-
-
-
-
-// render all 20 breweries 
-// function renderBeers(drink){
-//     console.log(drink)
-    // drink.forEach(drink => newElements(drink)) // use this one 
-
-//    
-//   }
 
 
 
