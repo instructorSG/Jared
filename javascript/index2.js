7ff097a178d2ad4cf851bc9b57227859ab3c2243
Jared McMahan <jaredm788@gmail.com>
2022-10-12 19:34:58 -0600

    created function with conditional statement

--- javascript/index2.js
+++ javascript/index2.js
@@ -1,102 +1,120 @@
 // request data from API 
 async function requestBreweriesData(citySearch) {
-  //Andrew help: why does the parm provide mulitple searches of the cities but with out parm it only returns 
-    await fetch(`https://api.openbrewerydb.org/breweries?by_city=${citySearch}`)
+  
+  fetch(`https://api.openbrewerydb.org/breweries?by_city=${citySearch}`)
     .then(response => response.json())
     .then(data => {
-        console.log("Breweries Array", data)
-        document.getElementById('breweryContainer').innerHTML = " "
-        data.forEach(breweries => {
-            renderBeerCards(breweries)
-            
-        })
-    })
-}
-
-function renderBeerCards(breweries) {
-    // console.log("BREWW", breweries)
-    const breweryCard = document.createElement('div')
-    const styleCard = breweryCard
-    styleCard.className = "card"
-    const brewName = document.createElement('h3')
-    const brewType = document.createElement("p")
-    const brewCity = document.createElement("p")
-    const brewState = document.createElement("p")
-    const likeBtn = document.createElement("button")
-    likeBtn.textContent = "Like!"
-    //handle mouse event to search by city  
-    likeBtn.addEventListener("click", () => {
-        // console.log("like click")
-        //invoking likeBrew function, passing in the obj argument
-        likeBrew(breweries)
-          
-        })
-    
-    brewName.textContent = breweries.name
-    brewType.textContent =  breweries.brewery_type
-    brewCity.textContent = breweries.city
-    brewState.textContent = breweries.state
-    
-    breweryCard.append(brewName,brewType, brewCity, brewState,likeBtn,)
-    document.getElementById('breweryContainer').append(breweryCard)
-          
-}
-
-//function to append. clearBtn setup
-function likeBrew(breweries) {
-  
-    const breweryCard = document.createElement('div')
-    const styleCard = breweryCard
-    styleCard.className = "card"
-    const brewName = document.createElement('h3')
-    const brewType = document.createElement("p")
-    const brewCity = document.createElement("p")
-    const brewState = document.createElement("p")
-    const clearBtn = document.createElement("button")
-    clearBtn.textContent = "clear"
-    clearBtn.className = "clear Button"
-    
-      brewName.textContent = breweries.name
-      brewType.textContent = breweries.brewery_type
-      brewCity.textContent = breweries.city
-      brewState.textContent = breweries.state
+      console.log("data", data)
+      document.getElementById('breweryContainer').innerHTML = " "
       
-      breweryCard.append(brewName,brewType, brewCity, brewState, clearBtn)
-      document.getElementById('Favs').appendChild(breweryCard)
-    }
-  
-// clear card from Favorites
-  function handleCardDelete() {
-      document.addEventListener("click", (e) => {
-        //provide if statement to provide the click with a target. 
-       if(e.target.classList.value === "clear Button") {
-        //use classList as alternative to grabbing a delimited space string of className
-              e.target.parentNode.remove()
-       }
+      data.forEach(brewery => {
+        renderBeerCards(brewery)
+        console.log("Breweries Array", brewery)
+
+      })
     })
-  }
-  handleCardDelete()
+}
+
+
+function renderBeerCards(brewery) {
+  // console.log("BREWW", brewery)
+  const breweryCard = createCard(brewery, "render")
+  document.getElementById('breweryContainer').append(breweryCard)
+
+}
+
+function likeBrew(brewery) {
+  const breweryCard = createCard(brewery, "clearBtn")
+
+  document.getElementById('Favs').appendChild(breweryCard)
+}
+
+
+
+
+
 
 //submit form based on user specified city and reset. 
 document.addEventListener('DOMContentLoaded', () => {
-    let form = document.getElementById('form')
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let citySearch = document.getElementById("city-input").value
-        requestBreweriesData(citySearch)
-        form.reset()
+  let form = document.getElementById('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let citySearch = document.getElementById("city-input").value
+    requestBreweriesData(citySearch)
+    form.reset()
 
-    })
+  })
 })
 
 //Event listener "change" for light/dark toggle
 function toggleSwitch() {
-    let darkToggle = document.querySelector('#darkToggle');
-      darkToggle.addEventListener('change', (e)=> { 
-      // console.log("toggg", e)
-      document.body.classList.toggle('dark')
+  let darkToggle = document.querySelector('#darkToggle');
+  darkToggle.textContent = "toggle"
+  darkToggle.addEventListener('change', (e) => {
+    // console.log("toggg", e)
+    document.body.classList.toggle('dark')
       // console.log(toggle)
       ;
-    });
+  });
+}
+toggleSwitch()
+
+
+
+
+
+
+
+function createCard(brewery, parm1) {
+//parm 1 determines rather I have a liked card or clear card. 
+
+  const breweryCard = document.createElement('div')
+  
+  breweryCard.className = "card"
+  const brewName = document.createElement('h3')
+  const brewType = document.createElement("p")
+  const brewLocation = document.createElement("p")
+ 
+  brewName.textContent = brewery.name
+  brewType.textContent = `Type: ${brewery.brewery_type}`
+  brewLocation.textContent = `${brewery.city}, ${brewery.state}`
+ 
+
+  breweryCard.append(brewName, brewType, brewLocation)
+
+  if (parm1 === "render") {
+    //this is the render container 
+
+    const likeBtn = document.createElement("button")
+    likeBtn.textContent = "Like!"
+    
+    likeBtn.addEventListener("click", () => {
+      // console.log("like click")
+      //invoking likeBrew function, passing in the obj argument
+      likeBrew(brewery)
+
+    })
+    breweryCard.append(likeBtn)
+  } else {
+    // this is the favorites container
+    const clearBtn = document.createElement("button")
+    clearBtn.textContent = "clear"
+    
+    clearBtn.addEventListener("click", (e) => {
+      // console.log("like click")
+      //invoking likeBrew function, passing in the obj argument
+      e.target.parentNode.remove()
+
+    })
+    breweryCard.append(clearBtn)
   }
-  toggleSwitch()
+
+  return breweryCard
+
+  //I  want to render createCard for both breweryContainer and Favs container
+  //solution: I want return createCard based on a conditional statement. If its renderBeer card
+  //return card with likeBtn
+  //or else return Favs card with clearBtn
+
+
+}
